12ce223795305168a4ca129d6a6bfcc8b6d1ceaf
Nook <Nook@DESKTOP-18E69G4>
2019-03-18 15:37:54 +0700

    First Commit

---
+++ testAPI/Controllers/InboxController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testAPI.Models;
+
+namespace testAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InboxController : ControllerBase
+    {
+        // GET: api/Inbox
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Inbox/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // GET: api/Inbox/
+        [HttpGet]
+        public ActionResult Get([FromBody]InboxViewModels model)
+        {
+            return Ok(model);
+        }
+
+        // POST: api/Inbox
+        [HttpPost]
+        public void Post([FromBody] InboxViewModels model)
+        {
+        }
+
+        // PUT: api/Inbox/5
+        [HttpPut]
+        public void Put([FromBody]InboxViewModels model)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ testAPI/Models/InboxViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testAPI.Models
+{
+    public class InboxViewModels
+    {
+        public string DocID { get; set; }
+        public string Date { get; set; }
+        public string Coproduct { get; set; }
+        public string Applicant { get; set; }
+        public string Activities { get; set; }
+        public string Plan { get; set; }
+        public string Subplan { get; set; }
+        public string Dealer { get; set; }
+        public string Category { get; set; }
+        public string Startdate { get; set; }
+        public string Enddate { get; set; }
+    }
+}
